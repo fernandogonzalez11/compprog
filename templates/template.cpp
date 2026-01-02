ca7993f1de1066a56baa25eb13ea9879c164e227
fernandogonzalez11 <fernandogonzalezeducativo@gmail.com>
2026-01-02 00:05:02 -0600

    new year new template

--- templates/template.cpp
+++ templates/template.cpp
@@ -1,22 +1,25 @@
 #include <bits/stdc++.h>
 
 // macros
-typedef long long int ll;
+#define int long long
 #define vec vector 
 #define loop(i, a, b) for (int i = a; i < b; i++)
 #define F first
 #define S second
 // constants
-#define INF (1LL << 62)
-#define int long long
 #define printarr(a) cout << #a << ": "; \
-	 	    for (auto x : a) cout << x << " "; \
-		    cout << "\n";
+	for (auto x : a) cout << x << " "; \
+	cout << "\n";
 
 using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+
+const int INF = 1LL << 62;
+const int MOD = 1e9 + 7;
 
 void solve() {
-    
+
 }
 
 signed main() {
